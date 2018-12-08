4c0254a92283129ed6e10f0843d39ccdd849aca5
AlejandroVelascoG <javier.velasco8@gmail.com>
2018-12-08 02:11:08 -0500

    pendiente: eliminar hojas 'Datos' de todos los archivos (for sobre las listas de hojas)

--- Excel SCF/lector.py
+++ Excel SCF/lector.py
@@ -20,35 +20,39 @@ import os
 #
 # hoja = archivo.sheet_by_name('logica')
 
+#nombres_hojas = []
 nombres = []
 apellidos = []
 institucion = []
 titulo = []
 tema = []
 
+#print(os.listdir('Mesas tematicas 2016'))
+
 for archivo in os.listdir('Mesas tematicas 2016'):
+	workbook = read.open_workbook(archivo)
+	nombres_hojas = workbook.sheet_names()
+	#nombres_hojas.extend(workbook.sheet_names())
+	print(nombres_hojas)
+	hoja = workbook.sheet_by_name(nombres_hojas[1]) # escoge la hoja relevante
+	
+	# crea las listas de nombres, apellidos, instituciones, titulos y temas
+	
+	# new_nombres = hoja.col_values(0, 1)
+	# new_apellidos = hoja.col_values(1, 1)
+	# new_institucion = hoja.col_values(4, 1)
+	# new_titulo = hoja.col_values(8, 1)
+	# new_tema = hoja.col_values(13, 1)
 
-    w_archivo = read.open_workbook(archivo) # abre el archivo
-    nombres_hojas = w_archivo.sheet_names() # crea lista de hojas
-    hoja = w_archivo.sheet_by_name(nombres_hojas[0]) # escoge la primera hoja
+	# nombres.extend(new_nombres)
+	# apellidos.extend(new_apellidos)
+	# institucion.extend(new_institucion)
+	# titulo.extend(new_titulo)
+	# tema.extend(new_tema)
 
-    # crea las listas de nombres, apellidos, instituciones, titulos y temas
+# columnas = pd.DataFrame({'1. Nombres': nombres, '2. Apellidos': apellidos, '3. Institución': institucion,
+#                         '4. Título de la actividad': titulo, '5. Temática': tema})
 
-    new_nombres = hoja.col_values(0, 1)
-    new_apellidos = hoja.col_values(1, 1)
-    new_institucion = hoja.col_values(6, 1)
-    new_titulo = hoja.col_values(8, 1)
-    new_tema = hoja.col_values(13, 1)
-
-    nombres.extend(new_nombres)
-    apellidos.extend(new_apellidos)
-    institucion.extend(new_institucion)
-    titulo.extend(new_titulo)
-    tema.extend(new_tema)
-
-columnas = pd.DataFrame({'1. Nombres': nombres, '2. Apellidos': apellidos, '3. Institución': institucion,
-                        '4. Título de la actividad': titulo, '5. Temática': tema})
-
-writer = ExcelWriter('ExcelSCF.xlsx')
-columnas.to_excel(writer,'Hoja1',index=False)
-writer.save()
+# writer = ExcelWriter('ExcelSCF.xlsx')
+# columnas.to_excel(writer,'Hoja1',index=False)
+# writer.save()
