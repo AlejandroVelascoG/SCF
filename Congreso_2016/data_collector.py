d58b1b8f85853b45c1cfbfc4ed4b0bdc11e2e3aa
AlejandroVelascoG <javier.velasco8@gmail.com>
2018-12-16 16:53:12 -0500

    archivos organizados

--- Congreso_2016/data_collector.py
+++ Congreso_2016/data_collector.py
@@ -2,8 +2,6 @@
 
 # ARCHIVO PARA LEER LOS ARCHIVOS Y COPIARLOS EN NUEVA HOJA
 
-# import os
-
 # diract = os.getcwd()
 
 # print(diract)
@@ -32,9 +30,22 @@ tema = []
 for archivo in os.listdir('Mesas tematicas 2016'):
 	workbook = read.open_workbook(archivo)
 	nombres_hojas = workbook.sheet_names()
-	#nombres_hojas.extend(workbook.sheet_names())
 	print(nombres_hojas)
-	hoja = workbook.sheet_by_name(nombres_hojas[1]) # escoge la hoja relevante
+	for i in nombres_hojas:
+		if i != 'Datos':
+			hoja = workbook.sheet_by_name(i)
+			new_nombres = hoja.col_values(0, 1)
+			new_apellidos = hoja.col_values(1, 1)
+			new_institucion = hoja.col_values(4, 1)
+			new_titulo = hoja.col_values(8, 1)
+			new_tema = hoja.col_values(13, 1)
+
+			nombres.extend(new_nombres)
+			apellidos.extend(new_apellidos)
+			institucion.extend(new_institucion)
+			titulo.extend(new_titulo)
+			tema.extend(new_tema)
+	 # escoge la hoja relevante
 	
 	# crea las listas de nombres, apellidos, instituciones, titulos y temas
 	
@@ -50,9 +61,9 @@ for archivo in os.listdir('Mesas tematicas 2016'):
 	# titulo.extend(new_titulo)
 	# tema.extend(new_tema)
 
-# columnas = pd.DataFrame({'1. Nombres': nombres, '2. Apellidos': apellidos, '3. Institución': institucion,
-#                         '4. Título de la actividad': titulo, '5. Temática': tema})
+columnas = pd.DataFrame({'1. Nombres': nombres, '2. Apellidos': apellidos, '3. Institución': institucion,
+                        '4. Título de la actividad': titulo, '5. Temática': tema})
 
-# writer = ExcelWriter('ExcelSCF.xlsx')
-# columnas.to_excel(writer,'Hoja1',index=False)
-# writer.save()
+writer = ExcelWriter('ExcelSCF.xlsx')
+columnas.to_excel(writer,'Hoja1',index=False)
+writer.save()
