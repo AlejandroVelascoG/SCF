b99ade04e61d0df3da578cfbd7cd7f607f10f7b0
AlejandroVelascoG <javier.velasco8@gmail.com>
2019-02-05 11:34:53 -0500

    programa mesas casi terminado

--- Congreso_2018/data_collector_mesas.py
+++ Congreso_2018/data_collector_mesas.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 nombres = []
 titulo = []
-afiliacion = []
+institucion = []
 lista_celdas = []
 
 workbook = read.open_workbook('Copia de Programación Mesas Temáticas FINAL.xlsx') # abre el archivo
@@ -24,23 +24,48 @@ for i in range(len(lista_hojas)): # por cada hoja en el libro
 
                 celda = columna[k] # elige la celda
                 #print(celda)
-                str = '' # crea un string vacío
+                dato = '' # crea un string vacío
                 contenido = [] # crea la lista que albergará los tres datos
+                i = 0
+                alm = 'Almuerzo'
 
+            
                 for c in celda: # por cada carácter en la celda
-                    if(celda != 'Almuerzo' and celda != ''):
-                        if c == celda[-1]: # si es el último carácter
-                            contenido.append(str) # mete el string en la lista de contenido
-                            lista_celdas.append(contenido) # mete la lista de contenido en la lista de todas las celdas
-                            contenido = [] # vacía la lista de contenido
-                            str = '' # vacía el string
-                        if c != '\n': # si el carácter no es un linebreak
-                            str += c # añade el carácter al string
-                        elif c == '\n': # si el carácter es un linebreak
-                            contenido.append(str) # añade el string a la lista de contenido
-                            str = '' # vacía el string
+                    if(alm not in celda and celda != ''):
+
+                    	##### SI ALM IN FILA-1 ?
 
 
-print(lista_celdas)
+                        if i == len(celda)-1: # si es el último carácte
+                       		dato += c
+                       		contenido.append(dato) # mete el string en la lista de contenido
+                       		lista_celdas.append(contenido) # mete la lista de contenido en la lista de todas las celdas
+                       		contenido = [] # vacía la lista de contenido
+                       		dato = '' # vacía el string
+                       	if c != '\n': # si el carácter no es un linebreak
+                       		dato += c # añade el carácter al string
+                       	elif c == '\n': # si el carácter es un linebreak
+                       		contenido.append(dato)
+                       		dato = '' # vacía el string
+                    i+=1
+
+
+#print(lista_celdas)
+
+#for i in lista_celdas:
+#	print len(i)
+
+for i in lista_celdas:
+	if len(i) == 4:
+		cat_ins = i[2] + ' / ' + i[3]
+		institucion.append(cat_ins)
+	elif len(i) == 3:
+		titulo.append(i[0])
+		nombres.append(i[1])
+		institucion.append(i[2])
+print(institucion)
+
+
+		
 # #lineas = celda.readlines()
 # print(lista)
